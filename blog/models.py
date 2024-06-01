9b7b6733e1d004e31a7f5f4db831256bee27e77e
ASKAR <askarjon.abdullayev@gmail.com>
2024-06-01 19:52:24 +0500

    Category, Tag and Post models are ready

---
+++ blog/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=500, verbose_name="Sarlavha")
+    body = models.TextField(blank=True, null=True)
+    cover = models.ImageField(upload_to="posts/images")
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    tag = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return self.title
