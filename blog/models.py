99c1a825f31025f03d4570ea218017e6f6447a29
ASKAR <askarjon.abdullayev@gmail.com>
2024-06-04 19:31:51 +0500

    default image in posts

--- blog/models.py
+++ blog/models.py
@@ -18,7 +18,7 @@ class Tag(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=500, verbose_name="Sarlavha")
     body = models.TextField(blank=True, null=True)
-    cover = models.ImageField(upload_to="posts/images")
+    cover = models.ImageField(upload_to="posts/images", blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
     tag = models.ManyToManyField(Tag, blank=True)
 
